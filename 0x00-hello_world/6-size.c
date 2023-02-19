e01884052efbf0a32dcad6ae66c1354540398cc0
lindalig <lindaliguru@gmail.com>
2023-02-19 12:34:00 -0800

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,4 +5,9 @@
  */
 int main(void)
 {
-	printf("
+	printf("Size of a char: 1 byte(s)\n", sizeo f(char));
+	printf("Size of an int: 4 byte(s)\n", sizeo f(int));
+	printf("Size of a long int: 4 byte(s)\n", sizeo f(long int));
+	printf("Size of a long long int: 8 byte(s)\n", sizeo f(long long int));
+	printf("Size of a float: 4 byte(s)\n", sizeo f(float));
+	return (0); }
