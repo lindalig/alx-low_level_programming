bf04301648e7cefddc6d67a71bbd9ef0eda628e6
lindalig <lindaliguru@gmail.com>
2023-02-26 11:13:30 -0800

    README.md

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ *Return; Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	_putchar("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	_putchar("%c: %d\n", c, _isupper(c));
+	return (0);
+}
