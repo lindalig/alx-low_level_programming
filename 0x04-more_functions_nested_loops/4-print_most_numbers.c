22808bb726743636aedaf735b4a107acb43d6910
lindalig <lindaliguru@gmail.com>
2023-02-26 13:42:39 -0800

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9
+ * except 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		if (ch != 2 && ch != 4)
+		{
+			_putchar(ch + '0');
+		}
+	}
+	_putchar('\n');
+}
