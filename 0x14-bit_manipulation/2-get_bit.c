2218ca1e05ec5da8fe71f0ecb1f17ab3bda451a0
lindalig <lindaliguru@gmail.com>
2023-04-09 13:03:41 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,10 +9,17 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_val;
+	unsigned int i;
 
-	if (index > 63)
-		return (-1);
-	bit_val = (n >> index) & 1;
-	return (bit_val);
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
 }
