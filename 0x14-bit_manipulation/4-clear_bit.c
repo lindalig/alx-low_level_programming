a2d839a9821ef78843ed547f053f3c91531498fe
lindalig <lindaliguru@gmail.com>
2023-04-09 12:48:36 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,15 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int m;
+
 	if (index > 63)
 		return (-1);
 
-	*n = ((1UL << index) & *n);
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
 	return (1);
 }
